b6e609fef6d2687a7a24eab06cfa685cf5f7cc4b
EC2 Default User <ec2-user@ip-10-0-1-49.ap-southeast-1.compute.internal>
2025-03-30 08:22:58 +0000

    testing

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from AWS CI/CD Pipeline!"
+    return "Hello from AWS CI/CD Pipeline! This is the first test I am having :)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
