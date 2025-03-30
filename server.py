a3772f58acb912c9718e1944c76f2ffafdb0f227
EC2 Default User <ec2-user@ip-10-0-1-49.ap-southeast-1.compute.internal>
2025-03-30 13:55:45 +0000

    last commit

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from AWS CI/CD Pipeline! test 4 surely this is the final commit"
+    return "<h1>please work please work please work please work 2</h1>" \
+           '<img src="/image/image.png" alt="My Image" width="300">'
+
+@app.route("/image/<filename>")
+def get_image(filename):
+    return send_from_directory("static/images", filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
+
